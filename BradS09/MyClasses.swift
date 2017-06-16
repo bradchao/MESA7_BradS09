0e85983699d07830bf11070ea40596b00ef78972
iii <iii@userteki-MacBook.local>
2017-06-16 09:33:01 +0800

    2017061603

---
+++ BradS09/MyClasses.swift
@@ -0,0 +1,22 @@
+//
+//  MyClasses.swift
+//  BradS09
+//
+//  Created by iii on 2017/6/16.
+//  Copyright © 2017年 iii. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01{}
+class MyClass02{
+    var x:Int
+    init(x:Int){self.x = x}
+}
+class MyClass03{
+    var x = 123
+    init(){}
+}
+class MyClass04{
+    var x:Int?
+}
